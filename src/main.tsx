ccb19ac8c2105ba8b2030e7cb3fe67c1432ce54c
Harsh Hemraz <harshhemraz403@gmail.com>
2026-08-20 15:25:19 +0400

    Feet: Kuma did the mistake not me

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { Analytics } from "@vercel/analytics/next"
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import App from './App'
 
 
